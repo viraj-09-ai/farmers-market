eb097cf5e74b5be7551792b00cc18aa8085924e3
viraj-09-ai <virajkalekar916@gmail.com>
2026-04-05 01:54:48 +0530

    fixed render port

--- backend/app.py
+++ backend/app.py
@@ -301,5 +301,7 @@ def update_status(id):
 def get_image(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port=int(os.environ.get("PORT", 10000))
+    app.run( host="0.0.0.0", port=port)
